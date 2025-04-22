2e93a86ae790d1f21edd0dba8c52ecfe47efa0f6
Ryan Tseng <ryan10328@gmail.com>
2025-04-23 01:36:03 +0800

    setup nativewind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,jsx,ts,tsx}",
   ],
+  presets: [require("nativewind/preset")],
   theme: {
     extend: {},
   },
