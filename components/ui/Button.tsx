e657c553e20274a6f561c6d998e900f3a39f5c20
Ryan Tseng <ryan10328@gmail.com>
2025-05-14 22:02:26 +0800

    use tailwind active: effect instead of using StyleSheet

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,58 +2,42 @@ import { ReactNode } from "react";
 import {
   View,
   Text,
-  Pressable,
   GestureResponderEvent,
-  StyleSheet,
+  TouchableOpacity,
 } from "react-native";
-import { GlobalStyles } from "../../constants/styles";
-import { StyleProps } from "react-native-reanimated";
 
 type ButtonProps = {
   children?: ReactNode;
   onPress?: (evt: GestureResponderEvent) => void;
   mode: string;
-  style: object;
+  className: string;
 };
 
-const Button = ({ children, onPress, mode, style }: ButtonProps) => {
+const Button = ({ children, onPress, mode, className }: ButtonProps) => {
   return (
-    <View style={style}>
-      <Pressable
-        onPress={onPress}
-        style={({ pressed }) => pressed && styles.pressed}
+    <TouchableOpacity
+      onPress={onPress}
+      className={`rounded-md p-2 bg-indigo-500 active:opacity-75 active:bg-indigo-100 ${className}`}
+    >
+      <View
+        className={[
+          "w-full",
+          "p-2",
+          "rounded-md",
+          mode === "flat" && "bg-transparent",
+        ].join(" ")}
       >
-        <View style={[styles.button, mode === "flat" && styles.flat]}>
-          <Text style={[styles.buttonText, mode === "flat" && styles.flatText]}>
-            {children}
-          </Text>
-        </View>
-      </Pressable>
-    </View>
+        <Text
+          className={[
+            "text-center",
+            mode === "flat" ? "color-indigo-200" : "text-white",
+          ].join(" ")}
+        >
+          {children}
+        </Text>
+      </View>
+    </TouchableOpacity>
   );
 };
 
 export default Button;
-
-const styles = StyleSheet.create({
-  button: {
-    borderRadius: 4,
-    padding: 8,
-    backgroundColor: GlobalStyles.colors.primary500,
-  },
-  flat: {
-    backgroundColor: "transparent",
-  },
-  buttonText: {
-    color: "white",
-    textAlign: "center",
-  },
-  flatText: {
-    color: GlobalStyles.colors.primary200,
-  },
-  pressed: {
-    opacity: 0.75,
-    backgroundColor: GlobalStyles.colors.primary100,
-    borderRadius: 4,
-  },
-});
